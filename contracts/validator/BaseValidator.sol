13e1651b269c753d3c0a4c578d48e6823d3b84b3
lienhage <lienhage@gmail.com>
2023-06-26 16:11:01 +0800

    add comments for ecdsa validator

--- contracts/validator/BaseValidator.sol
+++ contracts/validator/BaseValidator.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.18;
 import "../interfaces/IValidator.sol";
 import "../VersaWallet.sol";
 
+/**
+ * @title BaseValidator
+ * @dev Base contract for validator implementation.
+ */
 abstract contract BaseValidator is IValidator {
     event WalletInited(address indexed wallet);
     event WalletCleared(address indexed wallet);
@@ -12,11 +16,17 @@ abstract contract BaseValidator is IValidator {
 
     mapping(address => bool) _walletInited;
 
+    /**
+     * @dev Modifier to check if the wallet has already been initialized.
+     */
     modifier initializer {
         require(!_walletInited[msg.sender], "Has already inited");
         _;
     }
 
+    /**
+     * @dev Modifier to check if the validator is enabled for the caller wallet.
+     */
     modifier onlyEnabledValidator {
         require(
             VersaWallet(msg.sender).isValidatorEnabled(address(this)),
@@ -25,6 +35,10 @@ abstract contract BaseValidator is IValidator {
         _;
     }
 
+    /**
+     * @dev Initializes the wallet configuration.
+     * @param data The initialization data.
+     */
     function initWalletConfig(bytes memory data)
         external
         initializer
@@ -34,15 +48,32 @@ abstract contract BaseValidator is IValidator {
         emit WalletInited(msg.sender);
     }
 
+    /**
+     * @dev Clears the wallet configuration. Triggered when disabled by a wallet
+     */
     function clearWalletConfig() external onlyEnabledValidator {
         _clear();
         emit WalletCleared(msg.sender);
     }
 
+    /**
+     * @dev Internal function to handle wallet initialization.
+     * Subclass must implement this function
+     * @param data The initialization data.
+     */
     function _init(bytes memory data) internal virtual {}
 
+    /**
+     * @dev Internal function to handle wallet configuration clearing.
+     * Subclass must implement this function
+     */
     function _clear() internal virtual {}
 
+    /**
+     * @dev Checks if the specified wallet has been initialized.
+     * @param wallet The wallet address to check.
+     * @return A boolean indicating if the wallet is initialized.
+     */
     function isWalletInited(address wallet) external view returns(bool) {
         return _walletInited[wallet];
     }
--- contracts/validator/ECDSAValidator.sol
+++ contracts/validator/ECDSAValidator.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.18;
 
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "./BaseValidator.sol";
 import "../libraries/SignatureHandler.sol";
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
+/**
+ * @title ECDSAValidator
+ */
 contract ECDSAValidator is BaseValidator {
     using ECDSA for bytes32;
 
@@ -12,10 +15,19 @@ contract ECDSAValidator is BaseValidator {
 
     mapping (address => address) private _signers;
 
+    /**
+     * @dev Sets a new signer for the calling wallet.
+     * @param newSigner The address of the new signer.
+     */
     function setSigner(address newSigner) external onlyEnabledValidator {
         _setSigner(newSigner, msg.sender);
     }
 
+    /**
+     * @dev Internal function to set a new signer for a specific wallet.
+     * @param newSigner The address of the new signer.
+     * @param wallet The address of the wallet.
+     */
     function _setSigner(address newSigner, address wallet) internal {
         require(newSigner != address(0), "Invalid signer address");
         address oldSigner = _signers[wallet];
@@ -23,15 +35,28 @@ contract ECDSAValidator is BaseValidator {
         emit SignerSet(wallet, oldSigner, newSigner);
     }
 
+    /**
+     * @dev Initializes the wallet configuration for the calling wallet.
+     * @param data The initialization data containing the signer address.
+     */
     function _init(bytes memory data) internal override {
         (address signer) = abi.decode(data, (address));
         _setSigner(signer, msg.sender);
     }
 
+    /**
+     * @dev Clears the wallet configuration for the calling wallet.
+     */
     function _clear() internal override {
         delete _signers[msg.sender];
     }
 
+    /**
+     * @dev Validates the signature of a user operation.
+     * @param _userOp The user operation data.
+     * @param _userOpHash The hash of the user operation.
+     * @return validationData The validation data.
+     */
     function validateSignature(
         UserOperation calldata _userOp,
         bytes32 _userOpHash
@@ -63,6 +88,14 @@ contract ECDSAValidator is BaseValidator {
         );
     }
 
+    /**
+     * @dev Checks if a signature is valid for a given hash and wallet,
+     * this is used to support EIP-1271 protocol.
+     * @param hash The hash to validate the signature against.
+     * @param signature The signature to validate.
+     * @param wallet The address of the wallet.
+     * @return A boolean indicating whether the signature is valid or not.
+     */
     function isValidSignature(
         bytes32 hash,
         bytes calldata signature,
@@ -81,14 +114,31 @@ contract ECDSAValidator is BaseValidator {
         return validationData == 0 ? true : false;
     }
 
+    /**
+     * @dev Get the signer address for a given wallet.
+     * @param wallet The address of the wallet.
+     * @return The address of the signer.
+     */
     function getSigner(address wallet) external view returns(address) {
         return _signers[wallet];
     }
 
+    /**
+     * @dev Inherits from ERC165.
+     */
     function supportsInterface(bytes4 interfaceId) external pure returns (bool) {
         return interfaceId == type(IValidator).interfaceId;
     }
 
+    /**
+     * @dev Internal function to validate a signature.
+     * @param signer The address of the signer.
+     * @param signature The signature to validate.
+     * @param hash The hash to validate the signature against.
+     * @param validUntil The valid until timestamp.
+     * @param validAfter The valid after timestamp.
+     * @return The validation data indicating the result of the signature validation.
+     */
     function _validateSignature(
         address signer,
         bytes memory signature,
@@ -104,10 +154,26 @@ contract ECDSAValidator is BaseValidator {
         return _packValidationData(sigFailed, validUntil, validAfter);
     }
 
+    /**
+     * @dev Pack the validation data.
+     * @param sigFailed The signature validation result.
+     * @param validUntil The valid until timestamp.
+     * @param validAfter The valid after timestamp.
+     * @return The packed validation data.
+     */
     function _packValidationData(uint256 sigFailed, uint256 validUntil, uint256 validAfter) internal pure returns (uint256) {
         return sigFailed | validUntil << 160 | validAfter << (160 + 48);
     }
 
+    /**
+     * @dev Check the decoded signature.
+     * @param sigType The signature type.
+     * @param maxFeePerGas The maximum fee per gas.
+     * @param maxPriorityFeePerGas The maximum priority fee per gas.
+     * @param actualMaxFeePerGas The actual maximum fee per gas from the user operation.
+     * @param actualMaxPriorityFeePerGas The actual maximum priority fee per gas from the user operation.
+     * @return A boolean indicating whether the decoded signature is valid or not.
+     */
     function _checkDecodedSig(
         uint256 sigType,
         uint256 maxFeePerGas,

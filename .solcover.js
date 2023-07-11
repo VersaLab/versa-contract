48ef47aaa5493e1046c9321a354cf21f2ab43f1c
lienhage <lienhage@gmail.com>
2023-07-11 14:54:36 +0800

    update solcover

--- .solcover.js
+++ .solcover.js
@@ -1,3 +1,16 @@
 module.exports = {
-    skipFiles: []
+    skipFiles: [
+        "test/base/MockEntryPoint.sol",
+        "test/base/MockEntryPointManager.sol",
+        "test/base/MockHooksManager.sol",
+        "test/base/MockModuleManager.sol",
+        "test/base/MockValidatorManager.sol",
+        "test/other/MockToken.sol",
+        "test/plugin/MockHooks.sol",
+        "test/plugin/MockModule.sol",
+        "test/plugin/MockValidator.sol",
+        "paymaster/VersaVerifyingPaymaster.sol",
+        "handler/TokenCallbackHandler.sol",
+        "base/FallbackManager.sol"
+    ]
 };

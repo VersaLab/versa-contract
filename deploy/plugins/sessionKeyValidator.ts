26de1eb1a66edf33a7e5b137aa485f86c076b959
lienhage <lienhage@gmail.com>
2023-07-25 14:21:30 +0800

    add deploy scripts

---
+++ deploy/plugins/sessionKeyValidator.ts
@@ -0,0 +1,41 @@
+import { ethers } from "hardhat";
+import * as deployer from "../helper/deployer";
+import mumbaiAddresses from "../addresses/polygonMumbai.json";
+import scrollTestnetAddresses from "../addresses/scrollTestnet.json";
+import fs from "fs";
+
+async function deployWithAddresses(addresses: any) {
+    const sessionKeyValdiator = await deployer.deploySessionKeyValidator();
+    addresses.sessionKeyValdiator = sessionKeyValdiator.address;
+    return addresses;
+}
+
+async function main() {
+    const [signer] = await ethers.getSigners();
+    const network = await signer.provider?.getNetwork();
+
+    switch (network?.chainId) {
+        case 80001: {
+            const result = await deployWithAddresses(mumbaiAddresses);
+            console.log("writing changed address to output file 'deploy/addresses/polygonMumbai.json'");
+            fs.writeFileSync("deploy/addresses/polygonMumbai.json", JSON.stringify(result, null, "\t"), "utf8");
+            break;
+        }
+        case 534353: {
+            const result = await deployWithAddresses(scrollTestnetAddresses);
+            console.log("writing changed address to output file 'deploy/addresses/scrollTestnet.json'");
+            fs.writeFileSync("deploy/addresses/scrollTestnet.json", JSON.stringify(result, null, "\t"), "utf8");
+            break;
+        }
+        default: {
+            console.log("unsupported network");
+        }
+    }
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
